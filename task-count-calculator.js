a71d00f38fad7ef4df08e0e5bc590d94db5a489b
Barry Mullan <mullan@gmail.com>
2015-09-14 09:26:59 -0400

    added calculator

---
+++ task-count-calculator.js
@@ -0,0 +1,83 @@
+Ext.define("TaskCountCalculator", function() {
+
+    var self;
+
+    return {
+
+        extend: "Rally.data.lookback.calculator.TimeSeriesCalculator",
+
+        config : {
+        },
+
+        constructor:function(config) {
+            self = this;
+            this.initConfig(config);
+            return this;
+        },
+
+        getMetrics: function () {
+
+            return [
+                {
+                    "as": "Total",
+                    "field": "ObjectID",
+                    "display": "line",
+                    "f": "count"
+                },
+                {
+                    "field": "ObjectID",
+                    "as": "In-Progress",
+                    "f": "filteredCount",
+                    "filterField": "State",
+                    "filterValues": ["In-Progress"],
+                    "display": "column"
+                },
+                {
+                    "field": "ObjectID",
+                    "as": "Defined",
+                    "f": "filteredCount",
+                    "filterField": "State",
+                    "filterValues": ["Defined"],
+                    "display": "column"
+                },
+                {
+                    "field": "ObjectID",
+                    "as": "Completed",
+                    "f": "filteredCount",
+                    "filterField": "State",
+                    "filterValues": ["Completed"],
+                    "display": "column"
+                }
+            ];
+        },
+
+        getDerivedFieldsOnInput : function () {
+            // XS 1, S 3, M 5, L 8, XL 13
+            return [
+            ];
+        },
+
+        getDerivedFieldsAfterSummary : function () {
+
+            return [
+                {
+                    as: 'Ideal', f: function(row, index, summaryMetrics, seriesData) {
+                        // if (index===0) console.log(seriesData);
+                        var maxTasks = _.max( _.pluck( seriesData, "Total"));
+                        var increments = seriesData.length - 1;
+                        var incrementAmount = maxTasks / increments;
+                        // console.log(maxTasks,increments,incrementAmount);
+
+                        return (100 * (maxTasks - (index * incrementAmount)) / 100);
+                    }
+                }
+            ];
+
+        },
+
+        defined : function(v) {
+            return (!_.isUndefined(v) && !_.isNull(v));
+        }
+    };
+   
+});
